a1274518e25d37d4d794c95e05c31fea6d78c02f
DonatasNoreika <donoras@gmail.com>
2023-12-11 08:47:50 +0200

    Iš biudzetas.py iškelta konsolės logika

--- app.py
+++ app.py
@@ -6,13 +6,24 @@ while True:
     veiksmas = int(input("1 - įvesti pajamas\n2 - įvesti išlaidas\n3 - ataskaita\n4 - balansas\n0 - išeiti\n"))
     match veiksmas:
         case 1:
-            biudzetas.prideti_pajamu_irasa()
+            suma = float(input("Suma: "))
+            siuntejas = input("Siuntėjas: ")
+            info = input("Papildoma informacija: ")
+            biudzetas.prideti_pajamu_irasa(suma, siuntejas, info)
         case 2:
-            biudzetas.prideti_islaidu_irasa()
+            suma = float(input("Suma: "))
+            budas = input("Mokėjimo būdas: ")
+            isigyta = input("Įsigyta prekė/paslauga: ")
+            info = input("Papildoma informacija: ")
+            biudzetas.prideti_islaidu_irasa(suma, budas, isigyta, info)
         case 3:
-            biudzetas.gauti_ataskaita()
+            print("-------------------")
+            print("Ataskaita:")
+            for irasas in biudzetas.zurnalas:
+                print(irasas)
+            print("-------------------")
         case 4:
-            biudzetas.gauti_balansa()
+            print("Balansas:", biudzetas.gauti_balansa())
         case 0:
             break
         case _:
